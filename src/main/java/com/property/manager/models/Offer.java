08b75348ac1fa5cf8e83fd39bc5d754ee1bbfd4f
Martin Nestorov <mdinestorov@gmail.com>
2017-11-16 23:30:38 +0200

    Added Offer object

---
+++ src/main/java/com/property/manager/models/Offer.java
@@ -0,0 +1,73 @@
+package com.property.manager.models;
+
+public class Offer {
+
+	private int userId;
+	private int offerId;
+	private int propertyId;
+	private String offerType;
+	private double offerAmount;
+
+	public Offer() {
+
+	}
+
+	public Offer(int userId, int offerId, int propertyId, String offerType, double offerAmount) {
+
+		this.userId = userId;
+		this.offerId = offerId;
+		this.propertyId = propertyId;
+		this.offerType = offerType;
+		this.offerAmount = offerAmount;
+	}
+
+	public int getUserId() {
+
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+
+		this.userId = userId;
+	}
+
+	public int getOfferId() {
+
+		return offerId;
+	}
+
+	public void setOfferId(int offerId) {
+
+		this.offerId = offerId;
+	}
+
+	public int getPropertyId() {
+
+		return propertyId;
+	}
+
+	public void setPropertyId(int propertyId) {
+
+		this.propertyId = propertyId;
+	}
+
+	public String getOfferType() {
+
+		return offerType;
+	}
+
+	public void setOfferType(String offerType) {
+
+		this.offerType = offerType;
+	}
+
+	public double getOfferAmount() {
+
+		return offerAmount;
+	}
+
+	public void setOfferAmount(double offerAmount) {
+
+		this.offerAmount = offerAmount;
+	}
+}
